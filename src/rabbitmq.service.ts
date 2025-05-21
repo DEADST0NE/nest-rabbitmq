d5005290e48611a65797359c9db6a749ed2bc5e1
Ruslan Pakhlivanov <deadstone@list.ru>
2025-05-21 22:21:36 +0300

    fix: bug

--- src/rabbitmq.service.ts
+++ src/rabbitmq.service.ts
@@ -5,7 +5,7 @@ import {
   OnModuleInit,
   OnModuleDestroy,
 } from '@nestjs/common';
-import { Channel, connect, Connection } from 'amqplib';
+import { Channel, connect, Connection, Options } from 'amqplib';
 
 import {
   QueueOptions,
@@ -37,6 +37,15 @@ export class RabbitmqService implements OnModuleInit, OnModuleDestroy {
   private RECONNECT_TTL = 3000;
   private connection?: Connection;
   private state: RabbitReadyState = RabbitReadyState.closed;
+  private subscribes: Map<string, () => void> = new Map();
+  private exchanges: Map<string, () => void> = new Map();
+  private queues: Map<
+    string,
+    {
+      queue: string;
+      reset: () => void;
+    }
+  > = new Map();
 
   async onModuleInit() {
     await this.connect();
@@ -46,16 +55,26 @@ export class RabbitmqService implements OnModuleInit, OnModuleDestroy {
     await this.disconnect();
   }
 
-  async connect(): Promise<void> {
+  async connect() {
     this.stayAlive = true;
     if (this.connected || this.state === RabbitReadyState.connecting) {
       return;
     }
     this.state = RabbitReadyState.connecting;
     try {
-      this.connection = await connect(this.options.url, {
-        clientProperties: { connection_name: this.options.connectionName },
-      });
+      const url = new URL(this.options.url);
+      this.connection = await connect(
+        {
+          hostname: url.hostname,
+          port: parseInt(url.port, 10),
+          username: url.username,
+          password: url.password,
+          vhost: url.pathname.slice(1),
+        },
+        {
+          clientProperties: { connection_name: this.options.connectionName },
+        },
+      );
     } catch (err) {
       this.logger.error(`Rabbit mq connection failed`);
       this.logger.log(err);
@@ -75,6 +94,7 @@ export class RabbitmqService implements OnModuleInit, OnModuleDestroy {
     });
 
     this.connection.on('error', (error) => {
+      void this.reconnect();
       this.logger.error(`AMQP error `, error);
     });
 
@@ -82,6 +102,14 @@ export class RabbitmqService implements OnModuleInit, OnModuleDestroy {
     this.channel = await this.connection.createChannel();
     this.state = RabbitReadyState.opened;
     this.logger.debug(`Rabbit channel created`);
+
+    this.recoverySub();
+  }
+
+  async recoverySub() {
+    if (this.subscribes.size) this.subscribes.forEach((c) => c());
+    if (this.exchanges.size) this.exchanges.forEach((c) => c());
+    if (this.queues.size) this.queues.forEach((c) => c.reset());
   }
 
   async reconnect() {
@@ -89,7 +117,7 @@ export class RabbitmqService implements OnModuleInit, OnModuleDestroy {
     await this.connect();
   }
 
-  async disconnect(): Promise<void> {
+  async disconnect() {
     this.stayAlive = false;
     this.connection?.removeAllListeners();
     await this.channel?.close();
@@ -103,15 +131,29 @@ export class RabbitmqService implements OnModuleInit, OnModuleDestroy {
   }
 
   async assetExchange(name: string, options: ExchangeOptions) {
-    this.logger.debug(`Asset exchange ${name}`);
+    this.logger.debug(`Assert exchange ${name}`);
     await this.connect();
+
+    this.exchanges.set(name, () =>
+      this.channel.assertExchange(name, options.type, options),
+    );
+
     return await this.channel.assertExchange(name, options.type, options);
   }
 
-  async assertQueue(name: string, options: QueueOptions): Promise<string> {
+  async assertQueue(
+    name: string,
+    options: QueueOptions,
+    patterns: string[] = [],
+  ) {
+    await this.connect();
+
+    const found = this.queues.get(name);
+    if (found) {
+      return found.queue;
+    }
     this.logger.debug(`Assert queue ${name}`);
 
-    await this.connect();
     const queue = await this.channel.assertQueue(name, options);
 
     if (options.prefetchCount) {
@@ -119,19 +161,45 @@ export class RabbitmqService implements OnModuleInit, OnModuleDestroy {
     }
 
     if (options.exchange?.length) {
-      await this.channel.bindQueue(
-        queue.queue,
-        options.exchange,
-        options.pattern,
-      );
+      if (patterns?.length === 0) {
+        await this.channel.bindQueue(
+          queue.queue,
+          options.exchange,
+          options.pattern,
+        );
+      } else {
+        for (const pattern of patterns) {
+          await this.channel.bindQueue(queue.queue, options.exchange, pattern);
+        }
+      }
     }
+
+    this.queues.set(name, {
+      queue: queue.queue,
+      reset: () => this.assertQueue(name, options, patterns),
+    });
     return queue.queue;
+  }
+
+  async publish(
+    exchange: string,
+    routingKey: string,
+    data: { [key: string]: any },
+    options: Options.Publish | undefined = undefined,
+  ) {
+    const payload = JSON.stringify(data);
+    this.channel.publish(
+      exchange,
+      routingKey,
+      Buffer.from(payload, 'utf-8'),
+      options,
+    );
   }
 
   async sendToQueue(
     queue: string,
     data: { [key: string]: any },
-    options: MessageOptions,
+    options: MessageOptions | undefined = undefined,
   ) {
     const payload = JSON.stringify(data);
     this.channel.sendToQueue(queue, Buffer.from(payload, 'utf-8'), options);
@@ -139,9 +207,11 @@ export class RabbitmqService implements OnModuleInit, OnModuleDestroy {
 
   async subscribeToQueue(
     queue: string,
-    callback: QueueCallback,
+    callback: QueueCallback | null = null,
     options: QueueOptions = {},
-  ): Promise<string> {
+  ) {
+    this.logger.debug(`Subscribe queue ${queue}`);
+
     const consumer = await this.channel.consume(
       queue,
       async (msg) => {
@@ -150,10 +220,17 @@ export class RabbitmqService implements OnModuleInit, OnModuleDestroy {
         try {
           payload = JSON.parse(content);
         } catch (error) {
-          this.logger.debug(`Invalid format message from queue`, content);
+          this.logger.debug(
+            `Invalid format message from queue`,
+            content,
+            error,
+          );
           this.channel.ack(msg);
           return;
         }
+
+        if (!callback) return;
+
         await callback(
           payload,
           (ack: boolean) => {
@@ -170,6 +247,14 @@ export class RabbitmqService implements OnModuleInit, OnModuleDestroy {
         noAck: options.noAck,
       },
     );
+    this.subscribes.set(consumer.consumerTag, () =>
+      this.subscribeToQueue(queue, callback, options),
+    );
     return consumer.consumerTag;
+  }
+
+  async unsubscribeToQueue(consumerTag: string) {
+    this.subscribes.delete(consumerTag);
+    return await this.channel.cancel(consumerTag);
   }
 }
